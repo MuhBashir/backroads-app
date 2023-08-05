3e29837e9ca6f2bdee2c5d38c4f33b0c27dd0633
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-08-05 15:36:24 +0100

    backroads app first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navabar from './components/Navabar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Navabar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
